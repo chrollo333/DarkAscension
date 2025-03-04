588d85ae22f350a647c4ff798635af42c8a1ae4a
chrollo333 <lukasheinz777@gmail.com>
2025-03-04 11:37:23 +0100

    .

---
+++ frontend/src/pages/HomePage/components/Panels/PanelFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Panel from "./Panel";
+
+const Footer = () => {
+    //Panel data
+
+    const panelData = [
+        {
+            image: ''
+        }
+    ]
+}
