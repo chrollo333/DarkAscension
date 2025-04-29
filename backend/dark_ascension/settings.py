1acbee1fc71cdf724b5acd5365ee0dd457545626
chrollo333 <lukasheinz777@gmail.com>
2025-04-29 11:34:02 +0200

    add external database

--- backend/dark_ascension/settings.py
+++ backend/dark_ascension/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,12 +77,27 @@ WSGI_APPLICATION = 'dark_ascension.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+try:
+        DATABASES = {
+            'default': dj_database_url.parse("postgresql://darkascension_db_user:dV2egGB8Sbdv9h5w51fbCS1IA4t53QNC@dpg-d089k695pdvs739ju0ag-a/darkascension_db", conn_max_age=600)
+        }
+except Exception as e:
+        print("⚠️ Failed to parse DATABASE_URL, falling back to SQLite:", e)
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
